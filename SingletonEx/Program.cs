384aedccb8e4b762d25b52ea88173996f54819d5
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-03-09 17:47:40 +0530

    Add project files.

---
+++ SingletonEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Singleton fromMentors = Singleton.GetInstance;
+            fromMentors.PrintDetails("From Mentors");
+            Singleton fromInterns = Singleton.GetInstance;
+            fromInterns.PrintDetails("From Interns");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonEx/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonEx
+{
+    public sealed class  Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+
+        private  Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value" + counter.ToString());
+
+        }
+
+        public static Singleton GetInstance
+        {
+            get {
+
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+            
+        }
+
+        public void PrintDetails(string message)
+        {
+
+            Console.WriteLine(message);
+
+        }
+
+    }
+}
